13ba5d64cf9e351f10f3ed88061fe004bbafe44a
LuanaMora <luanamora32@gmail.com>
2018-09-14 20:16:35 -0300

    Model ImpedimentosDefinitivos Adicionado

---
+++ BancoSangueAPI/BancoSangueAPI/Model/ImpedimentosDefinitivos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Model
+{
+    public class ImpedimentosDefinitivos
+    {
+        public int codigo { get; set; }
+        public string descricao { get; set; }
+        public DateTime dataRegistro { get; set; }
+    }
+}
