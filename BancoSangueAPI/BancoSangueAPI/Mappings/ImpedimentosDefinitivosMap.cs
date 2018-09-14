9ad1506b6b013eee31b66fac35ed814a1efdb44e
LuanaMora <luanamora32@gmail.com>
2018-09-14 20:30:54 -0300

    Criado ImpedimentosDefinitivosMap

---
+++ BancoSangueAPI/BancoSangueAPI/Mappings/ImpedimentosDefinitivosMap.cs
@@ -0,0 +1,21 @@
+﻿using BancoSangueAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Mappings
+{
+    public class ImpedimentosDefinitivosMap : IEntityTypeConfiguration<ImpedimentosDefinitivos>
+    {
+        public void Configure(EntityTypeBuilder<ImpedimentosDefinitivos> builder)
+        {
+            builder.ToTable("impedimentosdefinitivos");
+            builder.Property(i => i.Codigo).HasColumnName("cd_impedimentosdefinitivos");
+            builder.Property(i => i.Descricao).HasColumnName("ds_impedimentosdefinitivos");
+            builder.Property(i => i.DataRegistro).HasColumnName("dt_impedimentosdefinitivos");
+        }
+    }
+}
