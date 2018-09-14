e140730f2aef732be41cf978cb89231a73939951
Eduardo Zils <eduardo.m.zils@gmail.com>
2018-09-14 20:07:45 -0300

    Criado model PontoColeta.cs

---
+++ BancoSangueAPI/BancoSangueAPI/Model/PontoColeta.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Model
+{
+    public class PontoColeta
+    {
+        public int Codigo { get; set; }
+        public string Descricao { get; set; }
+        public Endereco Endereco{ get; set; }
+    }
+}
