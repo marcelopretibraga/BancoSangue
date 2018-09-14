38ba3dbe700e3aa332c11cc9f5beba5d04f46bb4
josebergonsi <josebergonsi1234@outlook.com>
2018-09-14 20:33:39 -0300

    Feito Mapping  Requisitos Basicos

---
+++ BancoSangueAPI/BancoSangueAPI/Mappings/RequisitosBasicosMap.cs
@@ -0,0 +1,25 @@
+﻿using BancoSangueAPI.Model;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Mappings
+{
+    public class RequisitosBasicosMap
+    {
+        public class MunicipioMap : IEntityTypeConfiguration<Model.RequisitosBasicos>
+        {
+            public void Configure(EntityTypeBuilder<RequisitosBasicos> builder)
+            {
+                builder.ToTable("requisitosbasicos");
+                builder.Property(r => r.Codigo).HasColumnName("cd_requisito");
+                builder.Property(r => r.Descricao).HasColumnName("descricao");
+                builder.Property(r => r.Data).HasColumnName("data");
+            }
+       
+        }
+    }
+}
