fd2c9bb63c6c05602d2921634789b2bfc2398108
Eduardo Zils <eduardo.m.zils@gmail.com>
2018-09-14 21:27:54 -0300

    Criado Repositorys e Controller; Adicionado PontoColeta ao BancoSangueContext;

---
+++ BancoSangueAPI/BancoSangueAPI/Controllers/PontoColetaController.cs
@@ -0,0 +1,33 @@
+﻿using BancoSangueAPI.Model;
+using BancoSangueAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Controllers
+{
+    public class PontoColetaController : Controller
+    {
+        private readonly IPontoColetaRepository _pontoColetaRepository;
+
+        public PontoColetaController(IPontoColetaRepository pontoColetaRepository)
+        {
+            _pontoColetaRepository = pontoColetaRepository;
+        }
+
+        /// <summary>
+        /// Metodo que retorna todos os Municipios
+        /// </summary>
+        /// <returns>Lista de municipios</returns>
+        [HttpGet]
+        public IEnumerable<PontoColeta> GetAll() =>
+            _pontoColetaRepository.GetAll();
+
+        [HttpPost]
+        public void Save([FromBody] PontoColeta ponto) =>
+            _pontoColetaRepository.Save(ponto);
+
+    }
+}
