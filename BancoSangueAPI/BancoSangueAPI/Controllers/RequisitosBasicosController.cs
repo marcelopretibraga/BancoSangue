85d113aa2b2c99f1e98507bea93f0fef5a49db52
josebergonsi <josebergonsi1234@outlook.com>
2018-09-14 22:18:27 -0300

    falta termina o controller e startup

--- BancoSangueAPI/BancoSangueAPI/Controllers/RequisitosBasicosController.cs
+++ BancoSangueAPI/BancoSangueAPI/Controllers/RequisitosBasicosController.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using BancoSangueAPI.Model;
+using BancoSangueAPI.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BancoSangueAPI.Controllers
 {
-    public class RequisitosBasicosController
+    [Produces("application/json")]
+    [Route("api/RequisitosBasicos")]
+    public class RequisitosBasicosController: Controller
     {
+        private readonly IRequisitosRepository _requisitosRepository;
+        public RequisitosBasicosController(IRequisitosRepository requisitosRepository)
+        {
+            _requisitosRepository = requisitosRepository;
+        }
+        [HttpPost]
+        public void Save([FromBody] RequisitosBasicos requisitos) =>
+          _requisitosRepository.Save(requisitos);
+
+        [HttpDelete]
+        public void Delete([FromBody] RequisitosBasicos requisitos) =>
+        _requisitosRepository.Save(requisitos);
+
+        [HttpPut]
+        public void Update([FromBody] RequisitosBasicos requisitos) =>
+     _requisitosRepository.Save(requisitos);
     }
 }
