8b03a08f3e697a5ac7afaa171870eab5f4092802
josebergonsi <josebergonsi1234@outlook.com>
2018-09-14 21:22:39 -0300

    Feito IRequisitosRepository

---
+++ BancoSangueAPI/BancoSangueAPI/Repository/IRequisitosRepository.cs
@@ -0,0 +1,19 @@
+﻿using BancoSangueAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BancoSangueAPI.Repository
+{
+    public interface IRequisitosRepository : IDisposable
+   
+    {
+        void Save(RequisitosBasicos requisitosBasicos);
+        void Update(RequisitosBasicos requisitosBasicos);
+         void Delete(int codigo);
+
+        List<RequisitosBasicos> GetAll();
+        RequisitosBasicos GetById(int id);
+    }
+}
---
+++ BancoSangueAPI/BancoSangueAPI/Repository/RequisitosRepository.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BancoSangueAPI.Model;
+
+namespace BancoSangueAPI.Repository
+{
+    public class RequisitosRepository : IRequisitosRepository
+    
+{
+          private readonly BancoSangueContext _context;
+
+    public RequisitosRepository(BancoSangueContext context)
+    {
+        _context = context;
+    }
+
+        public void Delete(int codigo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<RequisitosBasicos> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public RequisitosBasicos GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(RequisitosBasicos requisitosBasicos)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(RequisitosBasicos requisitosBasicos)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
