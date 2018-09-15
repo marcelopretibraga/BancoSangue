04e1d7a0a8b423e2a20d49bf2a95fd9f6941f4d8
fcsamuel <felipecarvalhosamuel@gmail.com>
2018-09-14 22:16:37 -0300

    Alterado de class para interface IDisposable implementado

--- BancoSangueAPI/BancoSangueAPI/Repository/IDoadorRepository.cs
+++ BancoSangueAPI/BancoSangueAPI/Repository/IDoadorRepository.cs
@@ -1,11 +1,13 @@
-﻿using System;
+﻿using BancoSangueAPI.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BancoSangueAPI.Repository
 {
-    public class IDoadorRepository
+    public interface IDoadorRepository : IDisposable
     {
+        void Save(Doador Doador);
     }
 }
